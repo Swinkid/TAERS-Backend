7cf71bd1e2e4b7e85486078830ca0ef785c0656c
Alex Noble <swinkid@googlemail.com>
2017-02-12 14:48:41 +0000

    Added some TODO's Added status updating Added resource adding

--- routes/api.js
+++ routes/api.js
@@ -4,10 +4,13 @@ var router = express.Router();
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/tears');
 
-var location = require('../models/location');
+var Location = require('../models/location');
+var Resource = require('../models/resource');
 
 router.post('/location', function(req, res, next) {
-    var newLocation = location({
+    //TODO: Check & Validate
+
+    var newLocation = Location({
         device: req.body.device,
         lat: req.body.lat,
         long: req.body.long,
@@ -24,6 +27,56 @@ router.post('/location', function(req, res, next) {
 
         res.json("Update Completed");
     });
+});
+
+router.post('/device/add', function(req, res, next){
+    //TODO: Check & Validate
+
+    var newDevice = Resource({
+        device : req.body.device,
+        callsign : req.body.callsign,
+        status : 'OFFLINE',
+        type : req.body.resourceType,
+        lastUpdated : new Date()
+    });
+
+    newDevice.save(function (err) {
+        if (err) throw err;
+
+        if(err){
+            res.json("Internal Server Error");
+        }
+
+        res.json("Update Completed");
+    });
+});
+
+router.post('/device/status/update', function(req, res, next){
+    Resource.findOne({ device : req.body.device }, function(err, device) {
+        if(!err){
+            if(!device){
+                res.json("Error Updating Status");
+            }
+
+            device.status = req.body.status;
+            device.lastUpdated = new Date();
+
+            device.save(function (err) {
+                if(!err){
+                    res.json("Status Updated");
+                } else {
+                    res.json("Error updating status");
+                }
+            });
+
+        } else {
+            res.json("Error Updating Status");
+        }
+    });
+});
+
+router.post('/device/callsign/update', function(req, res, next){
+
 });
 
 router.get('/updates', function(req, res, next) {
