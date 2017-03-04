07b689a704edd2b9fc8491e8ca1acec1ecd87012
Alex Noble <swinkid@googlemail.com>
2017-03-04 22:51:32 +0000

    Fixed bodyparser dependancy

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var path = require('path');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
-
+var bodyParser = require('body-parser');
 var api = require('./routes/api');
 var frontend = require('./routes/frontend');
 
@@ -10,10 +10,12 @@ var app = express();
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
 app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.use('/api', api);
 app.use('/frontend', frontend);
