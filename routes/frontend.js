ea35352393d58e143ac346ba52db3ff775367ec1
Alex Noble <swinkid@googlemail.com>
2017-03-19 18:39:07 +0000

    Implemented travel time

--- routes/frontend.js
+++ routes/frontend.js
@@ -5,13 +5,23 @@ var _ = require('lodash');
 var Resource = require('../models/resource');
 
 router.get('/resource', function(req, res, next) {
-    Resource.find({}, function(err, resources){
-        if(!err){
-            res.json(resources);
-        } else {
-            res.json("Error");
-        }
-    });
+    if(req.query.status){
+        Resource.find({status: req.query.status}, function(err, resources){
+            if(!err){
+                res.json(resources);
+            } else {
+                res.json("Error");
+            }
+        });
+    } else {
+        Resource.find({}, function(err, resources){
+            if(!err){
+                res.json(resources);
+            } else {
+                res.json("Error");
+            }
+        });
+    }
 });
 
 
